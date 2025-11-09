052751b3b8912978fe580c8b3f7a0b505496af57
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-11-09 17:26:23 +0530

    lifecycle of bean using XML Configuration

--- src/main/java/com/springcore/lifecycle/MainApp.java
+++ src/main/java/com/springcore/lifecycle/MainApp.java
@@ -1,4 +1,14 @@
 package com.springcore.lifecycle;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MainApp {
+    public static void main(String[] args) {
+        AbstractApplicationContext con = new ClassPathXmlApplicationContext("lifecycle.xml");
+        Samosa s = (Samosa) con.getBean("s1");
+        System.out.println(s);
+        //  Registering shutdown Hook
+        con.registerShutdownHook();
+    }
 }
--- src/main/java/com/springcore/lifecycle/Samosa.java
+++ src/main/java/com/springcore/lifecycle/Samosa.java
@@ -1,4 +1,30 @@
 package com.springcore.lifecycle;
 
+
 public class Samosa {
+    private double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        System.out.println("setting price");
+        this.price = price;
+    }
+
+    public Samosa() {
+    }
+
+    @Override
+    public String toString() {
+        return "Samosa{" + "price=" + price + '}';
+    }
+
+    public void init(){
+        System.out.println("inside init method : Hey I am created");
+    }
+    public void destroy(){
+        System.out.println("Inside destroy method : I am going to die. You can't saved me");
+    }
 }
