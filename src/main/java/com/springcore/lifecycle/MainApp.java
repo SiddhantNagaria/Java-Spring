983a2609ca10d5446d74dc3bbc6b460b67ca508e
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-11-09 18:53:50 +0530

    Lifecycle of Bean using Interfaces

--- src/main/java/com/springcore/lifecycle/MainApp.java
+++ src/main/java/com/springcore/lifecycle/MainApp.java
@@ -10,5 +10,10 @@ public class MainApp {
         System.out.println(s);
         //  Registering shutdown Hook
         con.registerShutdownHook();
+
+        System.out.println("------------------------");
+        Pepsi p1 = (Pepsi)  con.getBean("pepsi1");
+        System.out.println(p1);
+
     }
 }
