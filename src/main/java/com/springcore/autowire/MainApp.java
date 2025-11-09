6e375ba0530bc95102cd226dc473b605adb16a54
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-11-09 22:08:08 +0530

    autowiring usnig XML

---
+++ src/main/java/com/springcore/autowire/MainApp.java
@@ -0,0 +1,13 @@
+package com.springcore.autowire;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext con = new ClassPathXmlApplicationContext("autowire.xml");
+        // autowire - byName
+        Emp e = con.getBean("emp1", Emp.class);
+        System.out.println(e);
+    }
+}
